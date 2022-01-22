7a7ecebadd5ebcd2e0f050097a7c512fa1068a38
Ramanjs <raman.s2662@gmail.com>
2022-01-23 00:57:35 +0530

    created ship factory function

---
+++ src/Ship.js
@@ -0,0 +1,18 @@
+export default function Ship() {
+  const length = len;
+  let isSunk = false;
+  const xCord = x;
+  const yCord = y;
+
+  const hit = (x, y) => {
+    return null;
+  }
+
+  const isSunk = () => {
+
+  }
+
+  return {
+    xCord, yCord, hit, isSunk
+  }
+}
