28e07670ddf41830ea4d57d84bb770efb459dbe2
Ramanjs <raman.s2662@gmail.com>
2022-02-14 16:03:05 +0530

    configure css loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,18 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
